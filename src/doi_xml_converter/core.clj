b0e449a41c24376dd45a8b89dcd86056ff0f3506
Bonnie Appleton <bemappleton@gmail.com>
2020-07-12 13:02:58 +0100

    Add response for a valid DOI that doesn't exist

--- src/doi_xml_converter/core.clj
+++ src/doi_xml_converter/core.clj
@@ -5,15 +5,19 @@
   (:gen-class))
 
 (defn article-json [req]
-  {:status  400
-   :headers {"Content-Type" "text/html"}
-   :body    (str "Invalid DOI: " (-> req :route-params :doi))})
+  (if (re-matches #"^10\.\d+\/\w+$" (-> req :route-params :doi))
+    {:status  404
+     :headers {"Content-Type" "text/json"}
+     :body    (str "DOI " (-> req :route-params :doi) " does not exist")}
+    {:status  400
+     :headers {"Content-Type" "text/json"}
+     :body    (str "Invalid DOI: " (-> req :route-params :doi))}))
 
 (defroutes app-routes
-           (GET "/works/:doi" [doi] article-json)
+           (GET "/works/:doi{.*}" [doi] article-json)
            (route/not-found "Error, page not found!"))
 
-(defn -main [& args]
+(defn -main []
   (let [port (Integer/parseInt (or (System/getenv "PORT") "8000"))]
     (server/run-server #'app-routes {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
--- test/doi_xml_converter/core_test.clj
+++ test/doi_xml_converter/core_test.clj
@@ -3,12 +3,16 @@
             [doi-xml-converter.core :refer :all]
             [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "should return 404 response for GET request to /not-an-endpoint"
+(deftest user-journey-tests
+  (testing "should return 404 response for GET request to an invalid endpoint"
     (let [response (app-routes (mock/request :get "/not-an-endpoint"))]
       (is (= 404 (:status response)))
       (is (= "Error, page not found!" (:body response)))))
-  (testing "should return 400 response for GET request to /works/not-a-doi"
+  (testing "should return 400 response for GET request with an invalid DOI"
     (let [response (app-routes (mock/request :get "/works/not-a-doi"))]
       (is (= 400 (:status response)))
-      (is (= "Invalid DOI: not-a-doi" (:body response))))))
+      (is (= "Invalid DOI: not-a-doi" (:body response)))))
+  (testing "should return 404 response for GET request to a valid DOI that does not exist"
+    (let [response (app-routes (mock/request :get "/works/10.5555/11111111"))]
+      (is (= 404 (:status response)))
+      (is (= "DOI 10.5555/11111111 does not exist" (:body response))))))
