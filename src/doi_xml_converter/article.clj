02d2bd92844f4818b65bb87cd9c4d96a2cca25f6
Bonnie Appleton <bemappleton@gmail.com>
2020-07-12 16:38:46 +0100

    Handle get request to crossref api

--- src/doi_xml_converter/article.clj
+++ src/doi_xml_converter/article.clj
@@ -1,4 +1,9 @@
-(ns doi-xml-converter.article)
+(ns doi-xml-converter.article
+  (:require [clj-http.client :as client]))
 
-(defn article-from-doi [doi] nil)
+(defn article-from-doi [doi]
+  (try (-> (str "https://api.crossref.org/v1/works/" doi "/transform/application/vnd.crossref.unixsd+xml")
+           client/get
+           :body)
+       (catch Exception e nil)))
 
--- test/doi_xml_converter/article_test.clj
+++ test/doi_xml_converter/article_test.clj
@@ -1,7 +1,12 @@
 (ns doi-xml-converter.article-test
   (:require [clojure.test :refer :all]
-            [doi-xml-converter.article :refer :all]))
+            [doi-xml-converter.article :refer :all]
+            [clj-http.client :as client]))
 
 (deftest article-from-doi-test
   (testing "should return nil if doi does not exist"
-    (is (= nil (article-from-doi "10.5555/1111111")))))
+    (with-redefs (client/get (fn [_] {:status 404}))
+      (is (= nil (article-from-doi "doi-does-not-exist")))))
+  (testing "should return article body if doi exists"
+    (with-redefs (client/get (fn [_] {:body "article-body"}))
+      (is (= "article-body" (article-from-doi "doi-exists"))))))
