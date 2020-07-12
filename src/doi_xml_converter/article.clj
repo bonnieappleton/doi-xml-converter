d240f8a30039589aafacfde81833defdcfe36bb5
Bonnie Appleton <bemappleton@gmail.com>
2020-07-12 17:30:39 +0100

    Add xml parsing

--- src/doi_xml_converter/article.clj
+++ src/doi_xml_converter/article.clj
@@ -1,9 +1,22 @@
 (ns doi-xml-converter.article
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.java.io :as io])
+  (:import (java.io ByteArrayInputStream)))
+
+(defn parse-xml [xml]
+  (-> xml
+      .getBytes
+      ByteArrayInputStream.
+      xml/parse
+      zip/xml-zip))
 
 (defn article-from-doi [doi]
   (try (-> (str "https://api.crossref.org/v1/works/" doi "/transform/application/vnd.crossref.unixsd+xml")
            client/get
-           :body)
+           :body
+           parse-xml
+           )
        (catch Exception e nil)))
 
--- test/doi_xml_converter/article_test.clj
+++ test/doi_xml_converter/article_test.clj
@@ -8,5 +8,17 @@
     (with-redefs (client/get (fn [_] {:status 404}))
       (is (= nil (article-from-doi "doi-does-not-exist")))))
   (testing "should return article body if doi exists"
-    (with-redefs (client/get (fn [_] {:body "article-body"}))
-      (is (= "article-body" (article-from-doi "doi-exists"))))))
+    (with-redefs (client/get (fn [_] {:body "<article>Hello world</article>"}))
+      (is (= [{:tag     :article
+               :attrs   nil
+               :content ["Hello world"]}
+              nil]
+             (article-from-doi "doi-exists"))))))
+
+(deftest parse-xml-test
+  (testing "should convert xml to clojure map"
+    (is (= [{:tag     :article
+             :attrs   nil
+             :content ["Hello world"]}
+            nil]
+           (parse-xml "<article>Hello world</article>")))))
